b95bc3d5bfb69f5945096f72f4bbf4b2202d1b38
keamanden <than0002@stud.kea.dk>
2025-10-03 10:55:30 +0200

    changed rest to controller

--- src/main/java/com/example/countries/controllers/Pages.java
+++ src/main/java/com/example/countries/controllers/Pages.java
@@ -1,16 +1,17 @@
 package com.example.countries.controllers;
 
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 
 public class Pages {
 
     @GetMapping("/welcome")
     public String welcome() {
-        return "/welcome";
+        return "welcome";
     }
 
 
